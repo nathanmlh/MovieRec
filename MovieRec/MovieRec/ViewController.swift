f377c8c5bc123587c470f7f2167b110a11790502
nathanmlh <nathanholdom1@gmail.com>
2021-04-13 11:35:38 -0400

    Initial commit

---
+++ MovieRec/MovieRec/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieRec
+//
+//  Created by Nathan Holdom on 4/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
