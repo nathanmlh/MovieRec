3f9b2695810c11ecb4f6c9bd7b0a2d6867287383
nathanmlh <nathanholdom1@gmail.com>
2021-04-14 19:26:55 -0400

    Add movie details page and like and dislike feature.

---
+++ MovieRec/Models/Movie.swift
@@ -0,0 +1,8 @@
+//
+//  Movie.swift
+//  MovieRec
+//
+//  Created by Nathan Holdom on 4/13/21.
+//
+
+import Foundation
---
+++ MovieRec/MovieRec/MovieCell.swift
@@ -0,0 +1,20 @@
+//
+//  MovieCell.swift
+//  MovieRec
+//
+//  Created by Nathan Holdom on 4/13/21.
+//
+
+import UIKit
+
+class MovieCell: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
